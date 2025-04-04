6423a94902534da209599ef79b200119761dfc17
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-04-05 05:17:15 +0530

    Initial layout created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
+import { AppBar, Toolbar, Typography, Button, Box, Container, CssBaseline} from "@mui/material";
 import Provider from "@/components/Provider";
+import Link from "next/link";
+
 
 export const metadata = {
   title: "Table Component Demo",
@@ -10,7 +13,38 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Provider>
-        {children}
+          <CssBaseline />
+
+          {/* Navigation Bar */}
+          <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>
+                    Table
+                  </Link>
+                </Typography>
+                <Button color="inherit" component={Link} href="/users">
+                  Users
+                </Button>
+                <Button color="inherit" component={Link} href="/products">
+                  Products
+                </Button>
+              </Toolbar>
+            </AppBar>
+
+            <Container maxWidth={false} sx={{ mt: 4 }}>
+              <Box sx={{ minHeight: 'calc(100vh - 120px)' }}>
+                {children}
+              </Box>
+
+              {/* Footer */}
+              <Box component="footer" sx={{ py: 3, textAlign: 'center', borderTop: '1px solid #eaeaea', mt: 4 }}>
+                <Typography variant="body2" color="text.secondary">
+                  {"Table Component by Manik :-)"}
+                </Typography>
+              </Box>
+            </Container>
+
         </Provider>
       </body>
     </html>
